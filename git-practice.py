2b8d128f03b8406cf34414dc5adc2942863a8326
Sophia Lam <sophia@lamfamilyonline.com>
2023-09-07 21:17:21 +0000

    delete function

--- git-practice.py
+++ git-practice.py
@@ -4,8 +4,21 @@ def addItem(item):
    toDoList.append(item)
    return toDoList
 
-userAns = input("Do you want to add to your list or quit? A/Q")
+def deleteItem(item):
+   toDoList.remove(item)
+   return toDoList
+
+userAns = input("Do you want to add to / remove from your list or quit? A/Q")
 while userAns == "A":
-   item = input("What item do you want to add?")
-   addItem(item)
-   userAns = input("Do you want to add to your list or quit? A/Q")
+   userAdd_Del = input("Do you want to add to or remove from your list? A/R")
+   if userAdd_Del == "A":
+      item = input("What item do you want to add?")
+      addItem(item)
+      userAns = input("Do you want to add to / remove from your list or quit? A/Q")
+   else:
+      item = input("What item would you like to remove?")
+      if item not in toDoList:
+         print("Error")
+      else:
+         deleteItem(item)
+         userAns = input("Do you want to add to / remove from your list or quit? A/Q")
